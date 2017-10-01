701f7a468de811ee3f85389864e3a155720370ad
UpTownCat <158668658@qq.com>
2017-10-01 22:15:43 +0800

    leetcode第三第四题

---
+++ LeetCode_004.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : LeetCode_004.py
+# @Author: UpTownCat
+# @Date  : 2017/10/1
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        mac = 1
+        right = 1
+        left = 0
+        length = len(s)
+        while left < length:
+            while right < length:
+                index = left
+                tag = 0
+                while index < right:
+                    if s[index] == s[right]:
+                        mac = max(mac, right - left)
+                        left = index + 1
+                        tag = 1
+                        break
+                    index = index + 1
+                right = right + 1
+                if tag:
+                    break
+            if right == length:
+                mac = max(mac, right - left)
+                break
+        if len(s) == 0:
+            mac = 0
+        return mac
