b4618ee8e9bf3d0d3a5a8e50e2c5ff793c98f865
xieshiwang <158668658@qq.com>
2017-12-23 17:53:25 +0800

    leetcode_84: 最大矩形面积

--- LeetCode_084.py
+++ LeetCode_084.py
@@ -4,14 +4,20 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        if not heights:
-            return 0
+        heights.append(0)
+        stack = []
         max_area = 0
-        left = []
-        right = []
-
+        for i, val in enumerate(heights):
+            if not stack or heights[stack[-1]] <= val:
+                stack.append(i)
+            else:
+                while(stack and heights[stack[-1]] > val):
+                    min_height = stack.pop()
+                    left = stack[-1] if stack else -1 # if stack is empty that is to say the current index is min of heights
+                    max_area = max(max_area, heights[min_height] * (i - left - 1))
+                stack.append(i)
+        heights.pop()
         return max_area
-
 solution = Solution()
-heights = [6, 2, 5, 4, 5, 1, 6]
+heights = [6]
 print(solution.largestRectangleArea(heights))
